007aa458fd8c2df9720d4ba35c7eb7e6af85e6a3
KapoorKartik <kapoorkartik110@gmail.com>
2023-12-25 22:50:57 +0530

    added breadcrumbs

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Tile } from "./Tile";
 import SearchBox from "../SearchBox";
 
-export const Test = () => {
+export const Listing = () => {
   const arr = [
     { subject: "HP joa it", validity: "11 Months", totalTest: "8" },
     { subject: "HP Cooprative Bank", validity: "1 Months", totalTest: "13" },
@@ -16,13 +16,10 @@ export const Test = () => {
     { subject: "HP TET (Medical)", validity: "2 Months", totalTest: "6" },
     { subject: "HP TET (Non-Medical)", validity: "5 Months", totalTest: "4" },
     { subject: "HP TET (Arts)", validity: "2 Months", totalTest: "9" },
+    { subject: "HP joa it", validity: "4 Months", totalTest: "12" },
   ];
   return (
     <>
-    <div className="sticky-top  text-start d-flex bg-light py-2">
-      <img className="ms-2" width="28" height="28" src="https://img.icons8.com/small/28/0d6efd/long-arrow-left.png" alt="l"/>
-      <div className="ms-2 fw-bold text-primary">Test Series</div>
-    </div>
       <SearchBox />
 
       {arr?.map(({ subject, validity, totalTest }, i) => {
