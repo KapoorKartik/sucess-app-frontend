9a72c1725d445f4d15876c7704373e5328da5ad7
Kartik Kapoor <kapoorkartk110@gmail.com>
2023-12-30 12:38:46 +0530

    working on question

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -5,8 +5,8 @@ import { Question } from "../components/Question";
 
 export const Questions = () => {
   const { mockId } = useParams();
-  const [qArr , setQarr] = useState([]);
-  const [currentQ , setCurrentQ] = useState(1);
+  const [qArr, setQarr] = useState([]);
+  const [currentQ, setCurrentQ] = useState(0);
   // console.log("mockID:", mockId);
 
   const getQuestionsByMockId = (mockId) => {
@@ -29,20 +29,55 @@ export const Questions = () => {
     }
   };
   useEffect(() => getQuestionsByMockId(mockId), []);
+  const handleQuestionChange = (val) => {
+    setCurrentQ((previous) => {
+      if (previous + val === qArr.length || previous + val < 0) {
+        console.log("this is last Question");
+        return previous;
+      } else {
+        return previous + val;
+      }
+    });
+  };
+
+  /* 
+  length = 3
+  currentQ = 0
+  handleQuestionChange(1)
+  currentQ = 1
+  handleQuestionChange(2)
+  currentQ = 2
+  handleQuestionChange(3)
+  no need to change currentQ
+*/
   return (
     <>
       <nav className="navbar navbar-light bg-primary liner-gradient mb-2 mx-0">
         <div className="container-fluid text-white">HP JOA IT</div>
       </nav>
       <div className="p-2 mb-2 mt-1 mx-2 bg-body rounded text-start d-flex justify-content-between">
-        <div>1 of 70</div>
-        <div>
-          {/* Timer <Timer timeInSec={50} /> */}
-        </div>
+        <div>{currentQ + 1} of 70</div>
+        <div>{/* Timer <Timer timeInSec={50} /> */}</div>
         <div>+1 -0.25</div>
       </div>
-      <Question questionObj={qArr[1]}/>
-      <button type="button"></button>
+      <Question questionObj={qArr[currentQ]} />
+      <div class="d-flex justify-content-between mt-2">
+        <button
+          onClick={() => handleQuestionChange(-1)}
+          type="button"
+          class="btn btn-primary"
+          disabled={currentQ === 0}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => handleQuestionChange(1)}
+          type="button"
+          class="btn btn-primary"
+        >
+          {currentQ + 1 === qArr.length ? "Submit" : "Save and Next"}
+        </button>
+      </div>
     </>
   );
 };
