c918d690197cfedde4b13f9d40b187383053b52b
Kartik Kapoor <kapoorkartk110@gmail.com>
2024-01-20 14:57:34 +0530

    update

--- src/pages/MockTestList.jsx
+++ src/pages/MockTestList.jsx
@@ -9,9 +9,7 @@ export const MockTestList = () => {
     //* this api mock the actual case api working
     //* sends the test code to fetch all the information about available mocks
     if (obj.testCode === "T1") {
-    
     } else if (obj.testCode === "T2") {
-
     }
   };
   return (
@@ -25,8 +23,16 @@ export const MockTestList = () => {
           alt="backBtn"
         />
       </Link>
-      <div className="ms-5 fs-2 fw-bold mt-1">{obj?.subject}</div>
+
+      <div className="ms-2 fs-4 fw-bold mt-1">{obj?.subject}</div>
       <div className="ms-2 fw-light text-muted">Validity: {obj?.validity}</div>
+      <div className="shadow p-2 mt-1 mx-3 bg-body rounded text-start mb-2 d-flex">
+        <div className="flex-grow-1 border">
+        <div>FLT #4</div>
+        <div className="text-muted" style={{fontSize : "11px"}}>Duration: 2hrs</div>
+        </div>
+        <img src="https://img.icons8.com/small/35/long-arrow-right.png" class="rounded-circle border" alt="exploerBtn"></img>
+      </div>
     </>
   );
   /*  
